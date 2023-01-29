eff18761648b8ec07a0b75b2ef21561d3f09cd21
Daria Zakhvey <Daria.Zakhvey@ihg.com>
2023-01-29 09:31:42 +0300

    init app

---
+++ src/test/java/bsuir/bank/system/BsuirBankSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package bsuir.bank.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsuirBankSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
